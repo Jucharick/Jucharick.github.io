3f7d99f59794a7ca91b7411e0a707d97028c2fa7
Jucharick <57427412+Jucharick@users.noreply.github.com>
2019-11-06 22:09:04 +0500

    Add files via upload

---
+++ spa/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+            rtl: false,
+            loop: true,
+            margin: 0,
+            items: 8,
+            merge: true,
+            StartPosition: "first_slide",
+            navigationText : ["",""],
+            slideBy : 2
+    });
+    var owl=$(".owl-carousel");
+	owl.owlCarousel();
+	//$(".next") - находим нашу кнопку
+	$(".carousel_right").click(function(){
+		owl.trigger("next.owl.carousel");
+	});
+	$(".carousel_left").click(function(){
+		owl.trigger("prev.owl.carousel");
+	});
+  });
