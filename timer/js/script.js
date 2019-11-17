8fde74336ff34e1819c1b5e38db7fb10e5d52730
Jucharick <57427412+Jucharick@users.noreply.github.com>
2019-11-17 14:40:03 +0500

    Update script.js

--- timer/js/script.js
+++ timer/js/script.js
@@ -96,7 +96,7 @@ function get_timer() {
 		setTimeout(get_timer,1000);
 	}
 	else {
-		$("#clock").html("<span id='stop'>Îòñ÷åò çàêîí÷åí!!!</span>");
+		$("#clock").html("<span id='stop'>Акция завершена!</span>");
 	}
 	
 }
