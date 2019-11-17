5dbe0ff96e1752a6b545d92f34a642f7ce4024c9
Jucharick <57427412+Jucharick@users.noreply.github.com>
2019-11-17 15:48:52 +0500

    Update script.js

--- timer/js/script.js
+++ timer/js/script.js
@@ -2,34 +2,47 @@ $(document).ready(function () {
 
 function get_timer() {
 	
-// 	var date_new = "December 31,2019 23:59";
+	// var date_new = "December 31,2019 23:59";
 
 	var date_t = new Date(2019,11,31,23,59,59);
 	var date = new Date();
 
 	var timer = date_t - date;
 	
-	if(date_t > date) {		
-		var day = parseInt(timer/(60*60*1000*24));
+
+	if(date_t > date) {	
+
+		// дней до даты_т делим на:60(час) 60(минута) 1000(секунда) 24(часов в сутках)	
+		var day = parseInt(timer/(60*60*1000*24)); // функция в числовой формат, откидывается дробная часть
 		if(day < 10) {
 			day = '0' + day;
-		}
-		day = day.toString();
-		var hour = parseInt(timer/(60*60*1000))%24;
+		} // если у нас 1 число, то к значение будет выводиться как 01	
+		day = day.toString(); // преобразуем в строку
+
+		// часов до даты_т делим на: 60(час) 60(минута) 1000(секунда)	
+		var hour = parseInt(timer/(60*60*1000))%24; // % - остаток от деления на 24 (чтоб не отображалось все кол-во часов до даты, т.к. дни (24) у нас учитываются в day)
 		if(hour < 10) {
 			hour = '0' + hour;
 		}
 		hour = hour.toString();
+		
+		// minutes
 		var min = parseInt(timer/(1000*60))%60;
 		if(min < 10) {
 			min = '0' + min;
 		}
 		min = min.toString();
+		
+		// seconds
 		var sec = parseInt(timer/1000)%60;
 		if(sec < 10) {
 			sec = '0' + sec;
 		}
 		sec = sec.toString();
+		
+
+
+		///////// day /////////
 		if(day[1] == 9 && 
 			hour[0] == 2 && 
 			hour[1] == 3 && 
@@ -39,6 +52,8 @@ function get_timer() {
 			sec[1] == 9) {
 			animation($("#day0"),day[0]);
 		}
+		// [] - обращаемся по индексу
+
 		else {
 			$("#day0").html(day[0]);
 		}
@@ -53,7 +68,10 @@ function get_timer() {
 		else {
 			$("#day1").html(day[1]);
 		}
-		if(hour[1] == 3 && 
+
+
+		///////// hour /////////
+		if(hour[1] == 3 && // 24 часа (при 3 в единицах часов происходит смена разряда)
 			min[0] == 5 && 
 			min[1] == 9 && 
 			sec[0] == 5 && 
@@ -72,9 +90,12 @@ function get_timer() {
 		else {
 			$("#hour1").html(hour[1]);
 		}
-		if(min[1] == 9 && 
-			sec[0] == 5 && 
-			sec[1] == 9) {
+		
+
+		///////// minutes /////////
+		if(min[1] == 9 &&  // учитываем так же секунды
+			sec[0] == 5 && // десятые секунд
+			sec[1] == 9) { // единица секунд
 			animation($("#min0"),min[0]);
 		}
 		else {
@@ -86,24 +107,34 @@ function get_timer() {
 		else {
 			$("#min1").html(min[1]);
 		}
+
+
+		///////// seconds /////////
 		if(sec[1] == 9) {
+			// десятые появляются когда единыца равна 9
 			animation($("#sec0"),sec[0]);
 		}
 		else {
 			$("#sec0").html(sec[0]);
 		}
 		animation($("#sec1"),sec[1]);	
+		
+
 		setTimeout(get_timer,1000);
 	}
 	else {
-		$("#clock").html("<span id='stop'>Акция завершена!</span>");
+		$("#clock").html("<span id='stop'>!</span>");
 	}
 	
 }
-function animation(vibor,param) {
-	vibor.html(param)
+
+
+function animation(option,param) {
+	option.html(param)
 		.css({'marginTop':'-20px','opacity':'0'})
 		.animate({'marginTop':'0px','opacity':'1'});
 }
+
+
 get_timer();
 });
